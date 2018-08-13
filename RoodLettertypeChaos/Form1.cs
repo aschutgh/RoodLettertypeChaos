52bae03b89e7ea2542a94dce0e167823e3b30487
Arthur Schut <aschutgh@gmail.com>
2018-08-13 15:20:57 +0200

    Add FIXME message

--- RoodLettertypeChaos/Form1.cs
+++ RoodLettertypeChaos/Form1.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+//FIXME: fontfamilies niet via Form1 constructor doorgeven
+
 namespace RoodLettertypeChaos
 {
     public partial class Form1 : Form
