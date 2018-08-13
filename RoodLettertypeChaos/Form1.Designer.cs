38cce32f02d77d8b61cec67a02ba63da28eacd28
Arthur Schut <aschutgh@gmail.com>
2018-08-13 12:50:13 +0200

    formulier ontwerp

--- RoodLettertypeChaos/Form1.Designer.cs
+++ RoodLettertypeChaos/Form1.Designer.cs
@@ -28,13 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ransomLetter = new System.Windows.Forms.RichTextBox();
+            this.convertButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(27, 71);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(660, 139);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Voer je losgeldbrief in:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // ransomLetter
+            // 
+            this.ransomLetter.Location = new System.Drawing.Point(27, 226);
+            this.ransomLetter.Name = "ransomLetter";
+            this.ransomLetter.Size = new System.Drawing.Size(660, 213);
+            this.ransomLetter.TabIndex = 2;
+            this.ransomLetter.Text = "";
+            // 
+            // convertButton
+            // 
+            this.convertButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.convertButton.Location = new System.Drawing.Point(568, 19);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(119, 32);
+            this.convertButton.TabIndex = 3;
+            this.convertButton.Text = "Converteer";
+            this.convertButton.UseVisualStyleBackColor = true;
+            this.convertButton.MouseClick += new System.Windows.Forms.MouseEventHandler(this.convertButton_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(718, 460);
+            this.Controls.Add(this.convertButton);
+            this.Controls.Add(this.ransomLetter);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "RoodLettertypeChaos";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox ransomLetter;
+        private System.Windows.Forms.Button convertButton;
     }
 }
 
--- RoodLettertypeChaos/Form1.cs
+++ RoodLettertypeChaos/Form1.cs
@@ -16,5 +16,25 @@ namespace RoodLettertypeChaos
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_MouseClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void convertButton_MouseClick(object sender, MouseEventArgs e)
+        {
+
+        }
     }
 }
